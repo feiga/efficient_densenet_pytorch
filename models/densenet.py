04c69f7e82486662efd6cffc4fc69ebdc8365999
feiga <gf0109@gmail.com>
2018-09-08 08:47:26 +0800

    hot fix

--- models/densenet.py
+++ models/densenet.py
@@ -45,14 +45,14 @@ class _Attention(nn.Module):
 
         return value
         
-def _atten_function_factory(norm, relu, conv, attention):
+def _atten_function_factory(attention):
     def bn_function(*inputs):
         #concated_features = torch.cat(inputs, 1)
         query = inputs[-1]
         candidates = torch.stack(inputs)
         atten_features = attention(query, candidates)
-        bottleneck_output = conv(relu(norm(atten_features)))
-        return bottleneck_output
+        # bottleneck_output = conv(relu(norm(atten_features)))
+        return atten_features #bottleneck_output
 
     return bn_function
 
@@ -66,27 +66,28 @@ def _bn_function_factory(norm, relu, conv):
 
 
 class _DenseLayer(nn.Module):
-    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate, efficient=False):
+    def __init__(self, num_input_features, bn_size, drop_rate, efficient=False):
         super(_DenseLayer, self).__init__()
         self.add_module('depth_attenion', _Attention(num_input_features)),
         self.add_module('norm1', nn.BatchNorm2d(num_input_features)),
         self.add_module('relu1', nn.ReLU(inplace=True)),
-        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size *
-                        growth_rate, kernel_size=1, stride=1, bias=False)),
+        self.add_module('conv1', nn.Conv2d(num_input_features,
+                        num_input_features, kernel_size=1, stride=1, bias=False)),
         self.add_module('norm2', nn.BatchNorm2d(bn_size * growth_rate)),
         self.add_module('relu2', nn.ReLU(inplace=True)),
-        self.add_module('conv2', nn.Conv2d(bn_size * growth_rate, growth_rate,
+        self.add_module('conv2', nn.Conv2d(num_input_features, num_input_features,
                         kernel_size=3, stride=1, padding=1, bias=False)),
         self.drop_rate = drop_rate
         self.efficient = efficient
 
     def forward(self, *prev_features):
-        bn_function = _bn_function_factory(self.norm1, self.relu1, self.conv1, self.depth_attention)
+        bn_function = _atten_function_factory(self.depth_attention)
         if self.efficient and any(prev_feature.requires_grad for prev_feature in prev_features):
-            bottleneck_output = cp.checkpoint(bn_function, *prev_features)
+            atten_features = cp.checkpoint(bn_function, *prev_features)
         else:
-            bottleneck_output = bn_function(*prev_features)
-        new_features = self.conv2(self.relu2(self.norm2(bottleneck_output)))
+            atten_features = bn_function(*prev_features)
+        new_features = self.conv1(self.relu1(self.norm1(atten_features)))
+        new_features = self.conv2(self.relu2(self.norm2(new_features)))
         if self.drop_rate > 0:
             new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
         return new_features
@@ -103,12 +104,11 @@ class _Transition(nn.Sequential):
 
 
 class _DenseBlock(nn.Module):
-    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate, efficient=False):
+    def __init__(self, num_layers, num_input_features, bn_size, drop_rate, efficient=False):
         super(_DenseBlock, self).__init__()
         for i in range(num_layers):
             layer = _DenseLayer(
-                num_input_features + i * growth_rate,
-                growth_rate=growth_rate,
+                num_input_features,
                 bn_size=bn_size,
                 drop_rate=drop_rate,
                 efficient=efficient,
@@ -120,7 +120,7 @@ class _DenseBlock(nn.Module):
         for name, layer in self.named_children():
             new_features = layer(*features)
             features.append(new_features)
-        return torch.cat(features, 1)
+        return features[-1] #torch.cat(features, 1)
 
 
 class DenseNet(nn.Module):
@@ -137,12 +137,13 @@ class DenseNet(nn.Module):
         small_inputs (bool) - set to True if images are 32x32. Otherwise assumes images are larger.
         efficient (bool) - set to True to use checkpointing. Much more memory efficient, but slower.
     """
-    def __init__(self, growth_rate=12, block_config=(16, 16, 16), compression=0.5,
-                 num_init_features=24, bn_size=4, drop_rate=0,
+    def __init__(self, 
+                 # growth_rate=12, # 
+                 block_config=(16, 16, 16),
+                 num_init_features=64, bn_size=4, drop_rate=0,
                  num_classes=10, small_inputs=True, efficient=False):
 
         super(DenseNet, self).__init__()
-        assert 0 < compression <= 1, 'compression of densenet should be between 0 and 1'
         self.avgpool_size = 8 if small_inputs else 7
 
         # First convolution
@@ -166,17 +167,16 @@ class DenseNet(nn.Module):
                 num_layers=num_layers,
                 num_input_features=num_features,
                 bn_size=bn_size,
-                growth_rate=growth_rate,
                 drop_rate=drop_rate,
                 efficient=efficient,
             )
             self.features.add_module('denseblock%d' % (i + 1), block)
-            num_features = num_features + num_layers * growth_rate
+            # num_features = num_features + num_layers * growth_rate
             if i != len(block_config) - 1:
                 trans = _Transition(num_input_features=num_features,
-                                    num_output_features=int(num_features * compression))
+                                    num_output_features= num_features * 2) #int(num_features * compression))
                 self.features.add_module('transition%d' % (i + 1), trans)
-                num_features = int(num_features * compression)
+                num_features = int(num_features * 2)
 
         # Final batch norm
         self.features.add_module('norm_final', nn.BatchNorm2d(num_features))
